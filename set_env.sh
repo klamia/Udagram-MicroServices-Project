a65f553be4bc876fa3056630c936eefdcc22bdfe
klamia <klamia17@gmail.com>
2021-09-06 09:11:55 +0100

    initial commit

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRESS_USERNAME = udagramkhettardv
+export POSTGRESS_PASSWORD = udagramkhettardv
+export POSTGRESS_DATABASE = udagramkhettardv 
+export POSTGRESS_HOST = udagramkhettardev.cttdeuvi9wgf.us-east-1.rds.amazonaws.com  
+export AWS_REGION = us-east-1
+export AWS_PROFILE = default
+export AWS_MEDIA_BUCKET = udagram-khettar-dev
+export JWT_SECRET = hello
+export URL=http://localhost:8100
